e20c263c3e6350cd4272d238bb57574f104a8ec9
Ryan Peters <ryanpeters@microsoft.com>
2024-02-21 21:27:53 -0500

    tweaks to app logging

--- Program.cs
+++ Program.cs
@@ -2,10 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Ok("looking good!"));
+// unauthenticated path
+app.MapGet("/", () =>
+{
+    const string successMessage = "well hello there anonymous user";
 
+    Console.WriteLine(successMessage);
+
+    return Results.Ok(successMessage);
+});
+
+// authenticated path
 app.MapGet("/secure", (string apikey) =>
 {
+    // our super-secret master key
+    const string key = "123456789";
+
     const string successMessage = "you passed in the correct key!";
     const string noKeyMessage = "no key provided";
     const string mismatchMessage = "key did not match";
@@ -17,7 +29,7 @@ app.MapGet("/secure", (string apikey) =>
         return Results.BadRequest(noKeyMessage);
     }
 
-    if (apikey == "123456789")
+    if (apikey == key)
     {
         Console.WriteLine(successMessage);
 
